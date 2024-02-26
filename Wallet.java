d736a7a769ed61f31b246c09fa403cb959f329a9
Dakoji-Durgaprasad <155511435+Dakoji-Durgaprasad@users.noreply.github.com>
2024-02-26 15:51:52 +0530

    Update Wallet.java

--- Wallet.java
+++ Wallet.java
@@ -2,8 +2,16 @@
 
 public class Wallet {
 
-	//
-	//
-	//
 	private int userId;
+	
+	private double AddMoney=0.0;
+
+	public double getAddMoney() {
+		return AddMoney;
+	}
+
+	public void setAddMoney(double addMoney) {
+		AddMoney = addMoney;
+	}
+	
 }
