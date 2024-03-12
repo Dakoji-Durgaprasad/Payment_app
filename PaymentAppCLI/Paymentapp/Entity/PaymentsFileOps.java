0d9b238cfb28c46254f1970e10b3a9d16b8a2f50
Dakoji-Durgaprasad <155511435+Dakoji-Durgaprasad@users.noreply.github.com>
2024-03-12 14:11:45 +0545

    File creation

---
+++ PaymentAppCLI/Paymentapp/Entity/PaymentsFileOps.java
@@ -0,0 +1,18 @@
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class PaymentsFileOps {
+	public final String usersFilePath = "G:\\Dp Folder\\PaymentAppfile\\Payments_CLI_USers.csv";
+	
+	public void writeUserToFile(User u) throws IOException {
+		File userFile = new File(usersFilePath);
+		FileWriter fw = new FileWriter(userFile,true);
+		fw.write(u.userToFileRecord());
+		
+		fw.flush();
+		fw.close();
+	}
+
+}
