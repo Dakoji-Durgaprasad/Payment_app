31672976f3990462fc7c4067755a4f1c779ff4bb
Dakoji-Durgaprasad <155511435+Dakoji-Durgaprasad@users.noreply.github.com>
2024-03-13 14:59:37 +0530

    Update Wallet.java

--- PaymentAppCLI/Paymentapp/Entity/Wallet.java
+++ PaymentAppCLI/Paymentapp/Entity/Wallet.java
@@ -3,10 +3,40 @@ public class Wallet {
 	
 	private int UserId;
 	
-	private int balance;
-	
+	private double currentBalance = 0;
+	private double walletAmountLimit =10000;
 	private double deposit;
 	private double withdraw;
-
+	
+	public int getUserId() {
+		return UserId;
+	}
+	public void setUserId(int userId) {
+		UserId = userId;
+	}
+	public double getCurrentBalance() {
+		return currentBalance;
+	}
+	public void setCurrentBalance(double currentBalance) {
+		this.currentBalance = currentBalance;
+	}
+	public double getWalletAmountLimit() {
+		return walletAmountLimit;
+	}
+	public void setWalletAmountLimit(double walletAmountLimit) {
+		this.walletAmountLimit = walletAmountLimit;
+	}
+	public double getDeposit() {
+		return deposit;
+	}
+	public void setDeposit(double deposit) {
+		this.deposit = deposit;
+	}
+	public double getWithdraw() {
+		return withdraw;
+	}
+	public void setWithdraw(double withdraw) {
+		this.withdraw = withdraw;
+	}
 	
 }
