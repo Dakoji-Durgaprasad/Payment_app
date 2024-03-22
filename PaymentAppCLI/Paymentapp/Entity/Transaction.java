f062290b8b69344701600ffc99da8735f32011b6
Dakoji-Durgaprasad <155511435+Dakoji-Durgaprasad@users.noreply.github.com>
2024-03-22 11:10:29 +0530

    Update Transaction.java

--- PaymentAppCLI/Paymentapp/Entity/Transaction.java
+++ PaymentAppCLI/Paymentapp/Entity/Transaction.java
@@ -1,167 +1,99 @@
 
+import java.time.LocalDate;
+import java.util.Date;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+public class Transaction {
 
-public class UserOperations {
-
-	public List<User> users = null;
-	public List<BankAccount> baAcctList = null;
-	public Map<Integer, Wallet> walletList = RunPaymentsApp.walletList;;
-
-	public UserOperations() {
-		users = RunPaymentsApp.userList;
-		baAcctList = RunPaymentsApp.baAcctList;
-		// walletList = RunPaymentsApp.walletList;
-	}
-
-	public User doUserRegistration(String fname, String lname, long phnum, String dob, String addr, String pswd) {
-		User u = new User();
-		u.setFirstName(fname);
-		u.setLastName(lname);
-		u.setPhoneNumber(phnum);
-		u.setDateofBirth(dob);
-		u.setAddress(addr);
-		u.setPassword(pswd);
-
-		u.setUserId((int) (Math.random() * 1000) + 100);
-
-		return u;
-	}
-
-	public void printUsersList(List<User> users) {
-		for (User u : users) {
-			if (users != null) {
-				System.out.println("User Details of : " + u.getFirstName());
-				System.out.println(u);
-			}
-		}
-	}
-
-	public boolean verifyUserLogin(String uId, String pswd) {
-		for (int i = 0; i < users.size(); i++) {
-			if (String.valueOf(users.get(i).getUserId()).equals(uId)) {
-				if (pswd.equals(users.get(i).getPassword())) {
-					return true;
-				}
-			}
-		}
-		return false;
-	}
-
-	public void printCurrUserDetails(int UserId) {
-		for (User u : users) {
-			if (u.getUserId() == UserId) {
-				System.out.println(u);
-			} else {
-				System.out.println("No user logged in");
-			}
-		}
-	}
-
-	public Map<User, List<BankAccount>> getUsersBankAccount() {
-
-		Map<User, List<BankAccount>> userBankAcctMap = new HashMap<User, List<BankAccount>>();
-
-		for (User u : users) {
-			if (users != null) {
-				userBankAcctMap.put(u, u.getBaList());
-			}
-		}
-		return userBankAcctMap;
-	}
-
-	public boolean verifyUserBankAccount(String bankAcctNumber, String pin) {
-		for (int i = 0; i < baAcctList.size(); i++) {
-			if (String.valueOf(baAcctList.get(i).getBankAcctNumber()).equals(bankAcctNumber)) {
-				if (pin.equals(baAcctList.get(i).getBankAcctPin())) {
-					baAcctList.remove(i);
-					return true;
-				}
-			}
-		}
-		return false;
-	}
-
-	public void addMoneyToWallet(double amount) {
-		if (walletList.containsKey(RunPaymentsApp.currUserId)) {
-			walletList.get(RunPaymentsApp.currUserId)
-					.setBalance(walletList.get(RunPaymentsApp.currUserId).getBalance() + amount);
-			System.out.println("Current wallet Balance: " + walletList.get(RunPaymentsApp.currUserId).getBalance());
-		}
-	}
-
-	public double checkWalletBalance() {
-		return walletList.get(RunPaymentsApp.currUserId).getBalance();
-	}
-
-//	public boolean transaction(Wallet sender, Wallet receiver, TxnType type, double amount) {
-//		if(walletList.get(RunPaymentsApp.currUserId).getBalance()>=amount) {
-//			walletList.get(RunPaymentsApp.currUserId)
-//			.setBalance(walletList.get(RunPaymentsApp.currUserId).getBalance() + amount);
-//			walletList.get(RunPaymentsApp.currUserId)
-//			.setBalance(walletList.get(RunPaymentsApp.currUserId).getBalance() - amount);
-//			return true;
-//		}
-//		return false;
-//	}
-	public boolean transaction(Wallet sender , Wallet receiver , TxnType type,double amount) {
-		if(sender.getBalance()>amount) {
-			receiver.setBalance(receiver.getBalance()+amount);
-			sender.setBalance(sender.getBalance()-amount);
-			return true;
-		}
-		return false;
-
-
-	}
-	public boolean transaction(BankAccount sender , BankAccount receiver , TxnType tType,double amount) {
-		if(sender.getBankBalance()>amount) {
-			receiver.setBankBalance(receiver.getBankBalance()+amount);
-			sender.setBankBalance(sender.getBankBalance()-amount);
-			return true;
-		}
-		return false;
-
-
-	}
-	public boolean transaction(BankAccount sender , Wallet receiver , TxnType type,double amount) {
-		if(sender.getBankBalance()>amount) {
-			receiver.setBalance(receiver.getBalance()+amount);
-			sender.setBankBalance(sender.getBankBalance()-amount);
-			return true;
-		}
-		return false;
-
-
-	}
-	public boolean transaction(Wallet sender , BankAccount receiver , TxnType type,double amount) {
-		if(sender.getBalance()>amount) {
-			receiver.setBankBalance(receiver.getBankBalance()+amount);
-			sender.setBalance(sender.getBalance()-amount);
-			return true;
-		}
-		return false;
-
-
-	}
-	public  void creditAmountToAccount(BankAccount bankAccount,double amount) {
-		bankAccount.setBankBalance(bankAccount.getBankBalance()+amount);
-		System.out.println("money added to your account");
-	}
+	private long txnId;
+	private Date txnDate;
+	private String Limit;
+	private BankAccount txnSource;
+	private BankAccount txnDestination;
+	private TxnType transactionType;
+	private double txnAmount;
+	private String statement;
 	
-	public static void sendMoneyWalletToWallet() {
+	private int userId;
+	
+	Wallet srcWallet;
+	Wallet destWallet;
+	public long getTxnId() {
+		return txnId;
+	}
+	public void setTxnId(long l) {
+		this.txnId = l;
+	}
+	public Date getTxnDate() {
+		return txnDate;
+	}
+	public void setTxnDate(Date date) {
+		this.txnDate = date;
+	}
+	public String getLimit() {
+		return Limit;
+	}
+	public void setLimit(String limit) {
+		Limit = limit;
+	}
+	public BankAccount getTxnSource() {
+		return txnSource;
+	}
+	public void setTxnSource(BankAccount source2) {
+		this.txnSource = source2;
+	}
+	public BankAccount getTxnDestination() {
+		return txnDestination;
+	}
+	public void setTxnDestination(BankAccount txnDestination) {
+		this.txnDestination = txnDestination;
+	}
+	public TxnType getTransactionType() {
+		return transactionType;
+	}
+	public void setTransactionType(TxnType transactionType) {
+		this.transactionType = transactionType;
+	}
+	public double getTxnAmount() {
+		return txnAmount;
+	}
+	public void setTxnAmount(double txnAmount) {
+		this.txnAmount = txnAmount;
+	}
+	public String getStatement() {
+		return statement;
+	}
+	public void setStatement(String statement) {
+		this.statement = statement;
+	}
+	public int getUserId() {
+		return userId;
+	}
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	public Wallet getSrcWallet() {
+		return srcWallet;
+	}
+	public void setSrcWallet(Wallet srcWallet) {
+		this.srcWallet = srcWallet;
+	}
+	public Wallet getDestWallet() {
+		return destWallet;
+	}
+	public void setDestWallet(Wallet destWallet) {
+		this.destWallet = destWallet;
+	}
+
+	public String toString(){
 		
-		
+		return "---" +": Transaction ID: " + txnId + " Date: " + txnDate 
+				+ " Amount: "+txnAmount + " Source Type: " + txnSource
+				+" Amount sent to UserId: "+ userId + " Destination Type: "
+				+ txnDestination ;
 	}
-
-	public static void sendMoneyBankToWallet() {
-
-	}
-
-	public static void sendMoneyBankToBank() {
-
-	}
-
 }
+
+
+
+
+
